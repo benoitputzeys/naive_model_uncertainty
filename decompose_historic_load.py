a191fc4e014332ea438f86a334995b48e86f4841
benoitputzeys <putzeys.benoit@gmail.com>
2021-06-13 22:30:02 +0200

    Refactoring and bugfix to compute the mean and stddev on a weekly basis.

--- decompose_historic_load.py
+++ decompose_historic_load.py
@@ -4,55 +4,53 @@ from functions import plot_figure
 
 # Get the processed data.
 y = pd.read_csv('data/data_preprocessing/y.csv')
-X = pd.read_csv('data/data_preprocessing/X.csv')
-X = X.set_index("Time")
-dates = X.index
+y = y.set_index("Time")
+dates = y.index
 series = y.iloc[:, -1]/1000
 
 # Compute the mean for each week
-# This section might take some time but calculating the mean for each is "safer" this way.
-# (If a value is missing in the original data, that is not a problem in computing the mean per week.)
 mean_each_week = series.copy()
 counter = 0
-for i in range(len(X)-1):
+for i in range(len(y)-1):
     mean_each_week[i-counter:i+1] = np.mean(series[i-counter:i+1])
     counter = counter + 1
-    if (X["Day of Week"][i] == 6) & (X["Day of Week"][i+1] == 0):
+    if (y["Day of Week"][i] == 6) & (y["Day of Week"][i+1]==0):
         counter = 0
-mean_each_week.iloc[-1] = mean_each_week.iloc[-2]
+mean_each_week.iloc[-1]=mean_each_week.iloc[-2]
 
 # Plot weekly mean
 ticks = ["2016-01", "2016-05", "2016-09",
         "2017-01", "2017-06", "2017-11",
         "2018-02", "2018-06", "2018-11",
         "2019-03", "2019-07", "2019-12", "2020-04"]
-#plot_figure.plot_single_fig(dates, mean_each_week, "Date", "Weekly Average, GW", ticks)
+plot_figure.plot_single_fig(dates, mean_each_week, "Date", "Weekly Average, GW", ticks)
 
-# Shift the load so that every week has a common point of reference
-shifted_load = X['Predicted_load'].values - mean_each_week
-shifted_load = pd.DataFrame({'SP': X['Settlement Period']+(48*X["Day of Week"]), 'Load': shifted_load.values})
+# Shift the load so that every week has a mean load of 0W
+shifted_load = series - mean_each_week
+
+# Modify the SP to go from 1 to 336 for each week (instead of 1 to 48 for each day)
+shifted_load = pd.DataFrame({'SP': y['Settlement Period']+(48*y["Day of Week"]), 'Load': shifted_load.values})
+
+# Split the data int training and test set
 shifted_load_train = shifted_load.iloc[31238:62476]
+shifted_load_test = shifted_load.iloc[62476:62476+7810]
 
 # Compute the mean and variation for each x.
 df_stats_train = pd.DataFrame({'Index': np.linspace(1, 336, 336), 'Mean': np.linspace(1, 336, 336), 'Stddev': np.linspace(1, 336, 336)})
+df_stats_test = pd.DataFrame({'Index': np.linspace(1, 336, 336), 'Mean': np.linspace(1, 336, 336), 'Stddev': np.linspace(1, 336, 336)})
 
+# Fill the respective dataframe with the mean/stddev of the respective settlement period (SP)
 for i in range(1,337):
     df_stats_train.iloc[i-1, 1] = np.mean(shifted_load_train[shifted_load_train["SP"] == i].iloc[:, -1])
     df_stats_train.iloc[i-1, 2] = np.std(shifted_load_train[shifted_load_train["SP"] == i].iloc[:, -1])
 
+for i in range(1, 337):
+    df_stats_test.iloc[i-1, 1] = np.mean(shifted_load_test[shifted_load_test["SP"] == i].iloc[:, -1])
+    df_stats_test.iloc[i-1, 2] = np.std(shifted_load_test[shifted_load_test["SP"] == i].iloc[:, -1])
+
 # Plot training
 ylabel = "Electricity Load Training Set, GW"
 plot_figure.plot_band(df_stats_train.iloc[:, 0], (+df_stats_train.iloc[:, 2]), ylabel, "blue", 0.2)
-
-# Use the test set data
-modified_timeseries_test = shifted_load.iloc[62476:62476+7810]
-
-# Compute the mean and variation for each x.
-df_stats_test = pd.DataFrame({'Index': np.linspace(1, 336, 336), 'Mean': np.linspace(1, 336, 336), 'Stddev': np.linspace(1, 336, 336)})
-
-for i in range(1, 337):
-    df_stats_test.iloc[i-1, 1] = np.mean(modified_timeseries_test[modified_timeseries_test["SP"] == i].iloc[:, -1])
-    df_stats_test.iloc[i-1, 2] = np.std(modified_timeseries_test[modified_timeseries_test["SP"] == i].iloc[:, -1])
 
 # Plot test data
 ylabel = "Electricity Load Test Set, GW"
